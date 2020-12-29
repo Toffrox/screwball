e2337bbff4af640bb074a9960faedd08c0ed6159
Toffrox <jeffrey.yflu@gmail.com>
2020-12-28 22:41:35 -0500

    changed spreadsheet to the copy

--- src/configureSheet.js
+++ src/configureSheet.js
@@ -3,7 +3,7 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 const creds = require ('./bk-members.json')
 
 // Config variables
-const SPREADSHEET_ID = '1L7-qEKxTZVPIHC2RKZflF6kRNJqQy0AF1CJ5zppLMQk';
+const SPREADSHEET_ID = '1Whax6iFkFs5-kBm6PdbwJAAk_oDajr50Y5s2eVENJzY';
 
 const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
 
